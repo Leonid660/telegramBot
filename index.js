965a308075a287ec4d38924526d7c23e1a2b3f0c
Leonid660 <leonid_zolotovky@bk.ru>
2022-03-31 15:43:00 +0300

    bot install

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const token = '5295398544:AAFgSmfgtNOaCU9sm4AW_KWmt0Y13BUs0mI'
 const bot = new TelegramApi(token, {polling: true})
 
 bot.on('message', nsg => {
-    
-    console.log(nsg)
+    const text = nsg.text;
+    const chatId = nsg.chat.id;
+
+    if(text === '/start'){
+        bot.sendMessage(chatId, `Добро пожаловать в телеграм бот`)
+    }
+    if (text === '/info'){
+        bot.sendMessage(chatId, `Тебя зовут ${nsg.from.first_name}${nsg.from.last_name}`)
+    }
+
 })
